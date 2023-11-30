13f2fadce366539f5abfac2163fc9cca930fb840
Marco Perini <perinim.98@gmail.com>
2023-11-30 19:30:04 +0100

    DEV added mqtt and fixed multiple connections

--- slave-raspberry/app/main.py
+++ slave-raspberry/app/main.py
@@ -2,6 +2,7 @@ import asyncio
 from bleak import BleakClient, BleakError, BleakScanner
 from decouple import config
 from influxdb_client import InfluxDBClient, Point, WriteOptions
+import paho.mqtt.client as paho
 from datetime import datetime
 import re
 
@@ -15,11 +16,27 @@ SENSORS_UUID = "19b10000-A001-537e-4f6c-d104768a1214" # UUID to read from
 nicla_address = ["EE:DF:46:E7:08:80", "9C:E3:E6:C9:4A:C8"]
 connected_address = None
 device_name = None
-client = None
+ble_client = None
+mqtt_client = None
 isStarted = False
-push2influxdb = False
+send2influxdb = False
+send2mqtt = True
 
-if push2influxdb:
+if send2mqtt:
+    # MQTT Settings
+    MQTT_ID = config('MQTT_ID', cast=str)
+    MQTT_HOST = config('MQTT_HOST', cast=str)
+    MQTT_PORT = config('MQTT_PORT', cast=int)
+
+    # Setup MQTT client
+    mqtt_client = paho.Client(client_id=MQTT_ID, clean_session=True, userdata=None, protocol=paho.MQTTv311, transport="tcp")
+
+    if mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60) != 0:
+        print("Couldn't connect to the mqtt broker")
+    else:
+        print("Connected to the mqtt broker")
+
+if send2influxdb:
     # InfluxDB Settings
     INFLUXDB_URL = config('INFLUXDB_URL', cast=str)
     INFLUXDB_TOKEN = config('INFLUXDB_TOKEN', cast=str)
@@ -27,10 +44,10 @@ if push2influxdb:
     INFLUXDB_BUCKET = config('INFLUXDB_BUCKET', cast=str)
 
     # Setup InfluxDB client
-    client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, debug=True, org=INFLUXDB_ORG)
+    influxdb_client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, debug=True, org=INFLUXDB_ORG)
 
     # Configure batch write client
-    write_api = client.write_api(write_options=WriteOptions(
+    write_api = influxdb_client.write_api(write_options=WriteOptions(
             batch_size=100,
             flush_interval=10_000,
             jitter_interval=2_000,
@@ -48,15 +65,20 @@ def write_to_influxdb(measurement, data, timestamp):
         point = point.field(key, value)
     write_api.write(INFLUXDB_BUCKET, INFLUXDB_ORG, point)
 
+# send through MQTT
+def send_to_mqtt(topic, data):
+    global mqtt_client
+    mqtt_client.publish(topic, data, 0)
+
 def notification_handler(sender: int, data: bytearray):
-    global pattern, client
+    global pattern, connected_address, send2mqtt
 
     raw_data = data.decode('utf-8')
     match = pattern.match(raw_data)
     if match:
         packet_id, g_x, g_y, g_z, a_x, a_y, a_z, q_x, q_y, q_z, q_w = match.groups()
         timestamp = datetime.utcnow()
-        if push2influxdb:
+        if send2influxdb:
             write_to_influxdb("movement_sensor_data", {
                 "packet_id": int(packet_id),
                 "gyro_x": float(g_x),
@@ -70,41 +92,45 @@ def notification_handler(sender: int, data: bytearray):
                 "quat_z": float(q_z),
                 "quat_w": float(q_w)
             }, timestamp)
+        if send2mqtt:
+            # print(f"nicla/{connected_address}/movement_sensor_data", f"{timestamp},{packet_id},{g_x},{g_y},{g_z},{a_x},{a_y},{a_z},{q_x},{q_y},{q_z},{q_w}")
+            send_to_mqtt(f"nicla/{connected_address}/movement_sensor_data", f"{timestamp},{packet_id},{g_x},{g_y},{g_z},{a_x},{a_y},{a_z},{q_x},{q_y},{q_z},{q_w}")
         
-        print(f"Packet ID: {packet_id}")
-        print(f"Gyroscope: [{g_x}, {g_y}, {g_z}]")
-        print(f"Accelerometer: [{a_x}, {a_y}, {a_z}]")
-        print(f"Quaternion: [{q_x}, {q_y}, {q_z}, {q_w}]")
+        # print(f"Packet ID: {packet_id}")
+        # print(f"Gyroscope: [{g_x}, {g_y}, {g_z}]")
+        # print(f"Accelerometer: [{a_x}, {a_y}, {a_z}]")
+        # print(f"Quaternion: [{q_x}, {q_y}, {q_z}, {q_w}]")
     else:
         print("Invalid data received:", raw_data)
 
 async def main_loop(address):
-    global isStarted, client, connected_address
+    global isStarted, ble_client, connected_address
     isStarted = False
     
     while True:
-        async with BleakClient(address) as client:
+        async with BleakClient(address, timeout=15) as ble_client:
             print("Connected successfully!")
             if not isStarted:
                 # send a byte 1 to start the program to the command characteristic
-                await client.write_gatt_char(PROGRAM_COMMAND_UUID, bytearray([1]))
-                await client.start_notify(SENSORS_UUID, notification_handler)
+                await ble_client.write_gatt_char(PROGRAM_COMMAND_UUID, bytearray([1]))
+                await ble_client.start_notify(SENSORS_UUID, notification_handler)
                 isStarted = True
 
-            while client.is_connected:
+            while ble_client.is_connected:
                 await asyncio.sleep(0.1)
             
             # exit the loop if client is disconnected
             print("Disconnected from device.")
             connected_address = None
+
             break
        
 async def main():
-    global nicla_address, connected_address, client, isStarted
+    global nicla_address, connected_address, ble_client, isStarted
     while True:
         try:
             # if client is not connected, scan for devices
-            if not client or not client.is_connected:
+            if not ble_client or not ble_client.is_connected:
                 connected_address = None
                 print("Scanning for devices...")
                 # scan for devices
@@ -115,13 +141,12 @@ async def main():
                             device_name = d.name
                             connected_address = d.address
                             print(f"Found {device_name} at {d.address}")
-                            await main_loop(connected_address)
                             break
-                    else:
-                        print("No SmartApples available found.")
+                    if connected_address is None:
+                        print("No SmartApples available found.")                   
         
             if connected_address is not None:
-                await main_loop(connected_address)        
+                await main_loop(connected_address)  
 
         except Exception as e:
             print(f"Unexpected error: {e}")
@@ -129,20 +154,15 @@ async def main():
             print("Interrupted by user.")
         finally:
             print("Disconnected, cleaning up...")
-            if client:
+            if ble_client:
                 try:
-                    await client.stop_notify(SENSORS_UUID)
-                    await client.disconnect()
-                    client = None
+                    await ble_client.stop_notify(SENSORS_UUID)
+                    await ble_client.disconnect()
+                    ble_client = None
                     connected_address = None
                     isStarted = False
                     await asyncio.sleep(1)
                 except Exception as e:
                     print(f"Error during cleanup: {e}")
 
-loop = asyncio.get_event_loop()
-
-try:
-    loop.run_until_complete(main())
-finally:
-    loop.close()
+asyncio.run(main())
