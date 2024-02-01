b9bb38dbc7f5fa11f3e9be3519529477112b646d
PeriniM <perinim.98@gmail.com>
2024-02-01 18:09:40 +0100

    dev: statistical study

--- data-analysis/process_data.py
+++ data-analysis/process_data.py
@@ -2,11 +2,12 @@ import os
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-
+import numpy as np
 # Get current directory and file directory
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 file_dir = os.path.join(curr_dir, 'acquisitions/onsite_test/raw')
-file_name = '10_smallapple_plant_2024-01-26.csv'
+processed_dir = os.path.join(curr_dir, 'acquisitions/onsite_test/processed')
+file_name = '5_apple_plant_2024-01-26.csv'
 # Read CSV file into a Pandas DataFrame
 df = pd.read_csv(file_dir+'/'+file_name)
 
@@ -37,55 +38,57 @@ numeric_columns = df.select_dtypes(include=['float64']).columns
 numeric_columns = numeric_columns.drop(['quat_x', 'quat_y', 'quat_z', 'quat_w', 'time_diff'])
 
 # Calculate the average of the initial 10 rows for each numeric feature
-initial_avg = df.head(10)[numeric_columns].mean()
-
-# Subtract the average values from the corresponding columns
+# initial_avg = df.head(10)[numeric_columns].mean()
+# print(initial_avg)
+# Subtract the average values for gravity compensation
 # df[numeric_columns] -= initial_avg
-
+# df['accel_x'] -= 0.5
+# df['accel_y'] += 1.0
+# df['accel_z'] -= 0.18
 # save the processed data to a new csv file
-df.to_csv(file_dir+'/processed_'+file_name, index=False)
+df.to_csv(processed_dir+'/processed_'+file_name, index=False)
 
 # Create a subplot with 3 rows and 1 column
-# fig = make_subplots(rows=3, cols=1)
+fig = make_subplots(rows=3, cols=1)
 
-# # Define dropdown buttons for each sensor type
-# accel_dropdown = [dict(label='X direction', method='update', args=[{'visible': [True, False, False, True, False, False, True, False, False]}, {'title': 'X direction'}]),
-#                   dict(label='Y direction', method='update', args=[{'visible': [False, True, False, False, True, False, False, True, False]}, {'title': 'Y direction'}]),
-#                   dict(label='Z direction', method='update', args=[{'visible': [False, False, True, False, False, True, False, False, True]}, {'title': 'Z direction'}])]
+# Define dropdown buttons for each sensor type
+accel_dropdown = [dict(label='X direction', method='update', args=[{'visible': [True, False, False, True, False, False, True, False, False]}, {'title': 'X direction'}]),
+                  dict(label='Y direction', method='update', args=[{'visible': [False, True, False, False, True, False, False, True, False]}, {'title': 'Y direction'}]),
+                  dict(label='Z direction', method='update', args=[{'visible': [False, False, True, False, False, True, False, False, True]}, {'title': 'Z direction'}])]
 
-# # Add traces for each sensor type
-# fig.add_trace(go.Scatter(x=df['_time'], y=df['accel_x'], mode='lines', name='accel_x', visible=True, connectgaps=False), row=1, col=1)
-# fig.add_trace(go.Scatter(x=df['_time'], y=df['accel_y'], mode='lines', name='accel_y', visible=True, connectgaps=False), row=1, col=1)
-# fig.add_trace(go.Scatter(x=df['_time'], y=df['accel_z'], mode='lines', name='accel_z', visible=True, connectgaps=False), row=1, col=1)
+# Add traces for each sensor type
+fig.add_trace(go.Scatter(y=df['accel_x'], mode='lines', name='accel_x', visible=True, connectgaps=False), row=1, col=1)
+fig.add_trace(go.Scatter(y=df['accel_y'], mode='lines', name='accel_y', visible=True, connectgaps=False), row=1, col=1)
+fig.add_trace(go.Scatter(y=df['accel_z'], mode='lines', name='accel_z', visible=True, connectgaps=False), row=1, col=1)
 
-# fig.add_trace(go.Scatter(x=df['_time'], y=df['gyro_x'], mode='lines', name='gyro_x', visible=True, connectgaps=False), row=2, col=1)
-# fig.add_trace(go.Scatter(x=df['_time'], y=df['gyro_y'], mode='lines', name='gyro_y', visible=True, connectgaps=False), row=2, col=1)
-# fig.add_trace(go.Scatter(x=df['_time'], y=df['gyro_z'], mode='lines', name='gyro_z', visible=True, connectgaps=False), row=2, col=1)
+fig.add_trace(go.Scatter(y=df['gyro_x'], mode='lines', name='gyro_x', visible=True, connectgaps=False), row=2, col=1)
+fig.add_trace(go.Scatter(y=df['gyro_y'], mode='lines', name='gyro_y', visible=True, connectgaps=False), row=2, col=1)
+fig.add_trace(go.Scatter(y=df['gyro_z'], mode='lines', name='gyro_z', visible=True, connectgaps=False), row=2, col=1)
 
-# fig.add_trace(go.Scatter(x=df['_time'], y=df['quat_x'], mode='lines', name='quat_x', visible=True, connectgaps=False), row=3, col=1)
-# fig.add_trace(go.Scatter(x=df['_time'], y=df['quat_y'], mode='lines', name='quat_y', visible=True, connectgaps=False), row=3, col=1)
-# fig.add_trace(go.Scatter(x=df['_time'], y=df['quat_z'], mode='lines', name='quat_z', visible=True, connectgaps=False), row=3, col=1)
+fig.add_trace(go.Scatter(x=df['_time'], y=df['quat_x'], mode='lines', name='quat_x', visible=True, connectgaps=False), row=3, col=1)
+fig.add_trace(go.Scatter(x=df['_time'], y=df['quat_y'], mode='lines', name='quat_y', visible=True, connectgaps=False), row=3, col=1)
+fig.add_trace(go.Scatter(x=df['_time'], y=df['quat_z'], mode='lines', name='quat_z', visible=True, connectgaps=False), row=3, col=1)
 
-# # Add dropdown buttons for each sensor type
-# fig.update_layout(
-#     updatemenus=[
-#         dict(type='buttons', showactive=True, buttons=[dict(label='Show All', method='update', args=[{'visible': [True]*9}, {'title': 'Show All'}])], x=0.2, xanchor='left', y=1.1, yanchor='top'),
-#         dict(type='dropdown', active=0, buttons=accel_dropdown, x=0.3, xanchor='left', y=1.1, yanchor='top'),
-#     ]
-# )
+# Add dropdown buttons for each sensor type
+fig.update_layout(
+    updatemenus=[
+        dict(type='buttons', showactive=True, buttons=[dict(label='Show All', method='update', args=[{'visible': [True]*9}, {'title': 'Show All'}])], x=0.2, xanchor='left', y=1.1, yanchor='top'),
+        dict(type='dropdown', active=0, buttons=accel_dropdown, x=0.3, xanchor='left', y=1.1, yanchor='top'),
+    ]
+)
 
-# # Update xaxis properties
-# fig.update_xaxes(title_text='Time', row=1, col=1)
-# fig.update_xaxes(title_text='Time', row=2, col=1)
-# fig.update_xaxes(title_text='Time', row=3, col=1)
+# Update xaxis properties
+fig.update_xaxes(title_text='Time', row=1, col=1)
+fig.update_xaxes(title_text='Time', row=2, col=1)
+fig.update_xaxes(title_text='Time', row=3, col=1)
 
-# # Update yaxis properties
-# fig.update_yaxes(title_text='Acceleration (g)', row=1, col=1)
-# fig.update_yaxes(title_text='Gyroscope (°/s)', row=2, col=1)
-# fig.update_yaxes(title_text='Quaternions', row=3, col=1)
+# Update yaxis properties
+fig.update_yaxes(title_text='Acceleration (g)', row=1, col=1)
+fig.update_yaxes(title_text='Gyroscope (°/s)', row=2, col=1)
+fig.update_yaxes(title_text='Quaternions', row=3, col=1)
 
-# # Update title and height
-# fig.update_layout(title_text='Sensor Data', height=1000)
+# Update title and height
+fig.update_layout(title_text='Sensor Data', height=1000)
 
-# # Show plot
-# fig.show()
+# Show plot
+fig.show()
